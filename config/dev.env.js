d6ba3aaddc36de69315fe593a02756295f3eb795
zhangzheming <v-zhangzhem.gd@chinatelecom.cn>
2019-03-14 14:44:27 +0800

    新增

---
+++ config/dev.env.js
@@ -0,0 +1,8 @@
+'use strict'
+const merge = require('webpack-merge')
+const prodEnv = require('./prod.env')
+
+module.exports = merge(prodEnv, {
+  NODE_ENV: '"development"',
+  BASE_API: '"https://www.easy-mock.com/mock/5c65167e2878863bc969ab12"',
+})
---
+++ src/api/gathering.js
@@ -0,0 +1,54 @@
+import request from '@/utils/request'
+
+const api_name = 'gathering'
+const group_name = 'gathering'
+export default {
+  getList() {
+    return request({
+      url: `/${group_name}/${api_name}`,
+      method: 'get'
+    })
+  },
+  search(page, size, searchMap) {
+    /*return request({
+      url:'/gathering/gathering/search/'+page+'/'+size,
+      method:'post',
+      data:searchMap
+    })*/
+    //使用es6模板字符串
+    return request({
+      url: `/${group_name}/${api_name}/search/${page}/${size}`,
+      method: 'post',
+      data: searchMap
+    })
+  },
+  save(pojo) {
+    return request({
+      url: `/${group_name}/${api_name}`,
+      method: 'post',
+      data: pojo
+    })
+  },
+  findById(id) {
+    return request({
+      url: `/${group_name}/${api_name}/${id}`,
+      method: 'get'
+    })
+  },
+  update(id, pojo) {
+    if (id == null || id == '') {
+      return this.save(pojo)
+    }
+    return request({
+      url: `/${group_name}/${api_name}/${id}`,
+      method: 'put',
+      data: pojo
+    })
+  },
+  deleteById(id) {
+    return request({
+      url: `/${group_name}/${api_name}/${id}`,
+      method: 'delete'
+    })
+  }
+}
